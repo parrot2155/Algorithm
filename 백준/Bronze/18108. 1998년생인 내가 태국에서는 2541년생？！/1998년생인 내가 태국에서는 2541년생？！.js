c4c91057e27c129cfc42940a480228960dd95534
parrot2155 <parrot2155@gmail.com>
2025-05-12 00:32:54 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 116 ms, Memory: 9740 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    year = Number(input[0]);
+
+console.log(year-543);
+});
