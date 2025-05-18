b83e7965d8d7aca2fa3d2ad844af524e077691f5
parrot2155 <parrot2155@gmail.com>
2025-05-18 14:20:39 +0900

    [Bronze V] Title: A+B - 7, Time: 208 ms, Memory: 18624 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Main main = new Main();
+		main.solution();
+	}
+	public void solution() {
+		Scanner sc = new Scanner(System.in);
+		int no = sc.nextInt();
+		
+		for(int i=1;i<=no;i++) {
+			int no1 = sc.nextInt();
+			int no2 = sc.nextInt();
+			System.out.println("Case #"+i+": "+(no1+no2));
+		}
+	}
+}
