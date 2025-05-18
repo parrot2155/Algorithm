32751b59949cd95a9ebf84149df6623ad103c7dd
parrot2155 <parrot2155@gmail.com>
2025-05-18 15:10:46 +0900

    [Bronze V] Title: 나머지, Time: 172 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Main main = new Main();
+		main.solution();
+	}
+	public void solution() {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.printf("%d\n%d\n%d\n%d", (a + b) % c, ((a % c) + (b % c)) % c, (a * b) % c, ((a % c) * (b % c)) % c);
+     }
+}
