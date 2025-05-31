157824656276aa10f5d10a17e3c51523e3bdc77a
parrot2155 <parrot2155@gmail.com>
2025-05-31 21:46:38 +0900

    [Bronze V] Title: 문자와 문자열, Time: 196 ms, Memory: 17780 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String[] str = sc.nextLine().split("");
+		int n = sc.nextInt();
+		
+		System.out.println(str[(n-1)]);
+		
+		
+		
+	}
+		
+}
