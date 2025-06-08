19c2642d1f287296b9d15150a6bfc768ab25f442
TW-Park <parrot2155@gmail.com>
2025-06-08 17:28:23 +0900

    Update 그룹 단어 체커.java

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -7,8 +7,8 @@ public class Main {
 		int n = sc.nextInt();
 		int cnt = 0;
 		for(int i=0;i<n;i++) {
-			StringBuffer str = new StringBuffer(sc.next());
-			for(int j=0;j<str.length();j++) {
+			StringBuffer str = new StringBuffer(sc.next());			//각 글자를 하나씩 읽어 현재 글자가 다음 글자랑 다르면 분리.
+			for(int j=0;j<str.length();j++) {				//-> 붙어있는 같은 글자끼리 분리함.
 				if(j<(str.length()-1)){
 					if(str.charAt(j)!=str.charAt(j+1)) {
 						str.insert(j+1," ");
@@ -18,8 +18,8 @@ public class Main {
 				
 			}
 			boolean chk = false;
-			String[] ch = str.toString().split(" ");
-			for(int j=0;j<ch.length;j++) {
+			String[] ch = str.toString().split(" ");		//분리된 각 글자들 중 같은 글자가 있는지 비교
+			for(int j=0;j<ch.length;j++) {				//있으면 카운트 안함.
 				
 				for(int k=(j+1);k<ch.length;k++) {
 						if(ch[j].charAt(0)==ch[k].charAt(0)){
