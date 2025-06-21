12b8c5a400c96fc26f5215a463cab916c0f80296
TW-Park <parrot2155@gmail.com>
2025-06-22 03:08:52 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 180 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a1 = sc.nextInt();
+		int a2 = sc.nextInt();
+		int a3 = sc.nextInt();
+		
+		if((a1+a2+a3)!=180) {
+			System.out.println("Error");
+		}else {
+			if(a1==60&&a2==60) {
+				System.out.println("Equilateral");
+			}else if(a1==a2||a2==a3||a3==a1){
+				System.out.println("Isosceles");
+			}else {
+				System.out.println("Scalene");
+			}
+		}
+	}
+}
