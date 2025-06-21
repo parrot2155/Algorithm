f26e2d29c4c8781e6470809ec051115318e6fa0d
TW-Park <parrot2155@gmail.com>
2025-06-22 03:48:44 +0900

    [Bronze II] Title: 커트라인, Time: 240 ms, Memory: 20800 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n =sc.nextInt();
+		int cut =sc.nextInt();
+		
+		int[] arr = new int[n];
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=sc.nextInt();
+		}
+		
+        for (int i=0; i<arr.length-1;i++) {		// 내림차순 정렬
+            for(int j=i+1; j<arr.length;j++) {
+                if(arr[i]<arr[j]) { 
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+		System.out.println(arr[cut-1]);
+	}
+}
