e97d52ad5640f5b8f3e67d46294ba4e3bdf87f1c
TW-Park <parrot2155@gmail.com>
2025-06-22 03:18:08 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 160 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			int a = sc.nextInt();
+	        int b = sc.nextInt();
+	        int c = sc.nextInt();
+	        
+	        if(a==0&&b==0&&c==0) {
+
+	        	break;
+	        }
+	        
+	        if(a>=(b+c)||b>=(c+a)||c>=(a+b)) {
+	        	System.out.println("Invalid");
+	        }else {
+	        	if(a==b&&b==c&&c==a) {
+	        		System.out.println("Equilateral");
+		        }else if(a==b||b==c||c==a) {
+		        	System.out.println("Isosceles");
+		        }else {
+		        	System.out.println("Scalene");
+		        }
+	        }
+		}
+		
+	}
+}
