7ec25cbd6857ef7e9c9a6d31de63a10228b9742c
TW-Park <parrot2155@gmail.com>
2025-06-28 23:33:26 +0900

    Update 알고리즘 수업 － 알고리즘의 수행 시간 4.java

--- 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -6,8 +6,16 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		long n = sc.nextInt();
 
-		System.out.println(n*(n-1)/2);
+		System.out.println(n*(n-1)/2);	//등차수열의 합
         System.out.println(2);
 	}
 	
 }
+//234567	6
+//34567		5
+//4567		4
+//567		3
+//67		2
+//7		1
+
+//		n 
