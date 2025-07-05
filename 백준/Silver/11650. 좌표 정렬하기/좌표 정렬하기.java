ac68326377e086e7554c51e273e6abbcde932506
TW-Park <parrot2155@gmail.com>
2025-07-06 01:52:00 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1844 ms, Memory: 185204 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); 
+        Coor[] arr = new Coor[n];
+
+        for (int i=0;i<n;i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            arr[i] = new Coor(x, y);
+        }
+        Arrays.sort(arr, new Comparator<Coor>() {
+            @Override
+            public int compare(Coor a, Coor b) {
+                if (a.getX() != b.getX()) {
+                    return a.getX() - b.getX();
+                } else {
+                    return a.getY() - b.getY();
+                }
+            }
+        });
+
+        for (Coor c : arr) {
+            System.out.println(c.getX()+" "+c.getY());
+        }
+    }
+
+    static class Coor {
+        private int x;
+        private int y;
+
+        public Coor(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+		public int getX() {
+			return x;
+		}
+
+		public void setX(int x) {
+			this.x = x;
+		}
+
+		public int getY() {
+			return y;
+		}
+
+		public void setY(int y) {
+			this.y = y;
+		}
+        
+    }
+}
