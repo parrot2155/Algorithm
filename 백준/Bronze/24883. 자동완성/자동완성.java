d90bf58f0a475833aaef9d609695abd76a7a017b
TW-Park <parrot2155@gmail.com>
2025-07-20 17:07:32 +0900

    [Bronze V] Title: 자동완성, Time: 172 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String in = sc.next();
+        
+        if(in.equals("N")||in.equals("n")) {
+        	System.out.println("Naver D2");
+        }else {
+        	System.out.println("Naver Whale");
+        }
+        
+    }
+}
