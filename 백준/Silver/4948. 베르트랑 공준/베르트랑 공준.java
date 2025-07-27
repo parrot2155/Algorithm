70eaee6355ea6cfa90ffebb6169376572ffaa0c8
TW-Park <parrot2155@gmail.com>
2025-07-28 00:35:24 +0900

    Update 베르트랑 공준.java

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		
-		Scanner scanner = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
         
-        //에라토스테네스 체
+        //에라토스테네스 체	
 		while(true) {
 			int cnt = 0;
 			int i,j=0;
-			int n = scanner.nextInt();
-			if(n==0) {
+			int n = sc.nextInt();
+			if(n==0) {	//0인 경우 종료
 				break;
 			}
 			boolean list[] = new boolean[2*n + 1];
 			Arrays.fill(list,true);
 			
-			list[1] = false;
+			list[1] = false;	//1은 소수 아님
 			
 			for (i = 2; i <= n*2; i++)
 				for (j = 2; i*j <= n*2; j++)
