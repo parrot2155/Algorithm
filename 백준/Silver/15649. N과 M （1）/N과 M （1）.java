0859c593e10d87cafb2977ff809af34bbdc490ea
TW-Park <parrot2155@gmail.com>
2025-08-17 21:38:28 +0900

    Update N과 M （1）.java

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,7 +1,20 @@
+package com.main;
+
 import java.util.Scanner;
 
+//1. 숫자를 하나 뽑는다.
+//
+//2. 뽑은 숫자는 used 배열에 표시해서 다시 못 쓰게 한다.
+//
+//3. 다음 자리로 넘어가서 또 숫자를 뽑는다.
+//
+//4. M개를 다 뽑으면 출력한다.
+//
+//5. 끝났으면 돌아와서(백트래킹) 방금 뽑았던 숫자를 다시 “안 쓴 상태”로 돌려놓는다.
+
 public class Main {
-    static int n, m;
+    static int n;
+    static int m;
     static boolean[] used;
     static int[] pick;
 
@@ -40,3 +53,5 @@ public class Main {
 
     
 }
+
+
