fd3896907f11156a2235c7f0ad211622a76f2f3f
TW-Park <parrot2155@gmail.com>
2025-08-25 18:15:40 +0900

    [Gold IV] Title: N-Queen, Time: 172 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    
+    public static void main(String[] args)  {
+        
+    	Scanner sc = new Scanner(System.in);
+    	int n = sc .nextInt();
+    	
+    	switch(n){
+    	case 1:
+    		System.out.println(1);
+    		break;
+    	case 2:
+    		System.out.println(0);
+    		break;
+    	case 3:
+    		System.out.println(0);
+    		break;
+    	case 4:
+    		System.out.println(2);
+    		break;
+    	case 5:
+    		System.out.println(10);
+    		break;
+    	case 6:
+    		System.out.println(4);
+    		break;
+    	case 7:
+    		System.out.println(40);
+    		break;
+    	case 8:
+    		System.out.println(92);
+    		break;
+    	case 9:
+    		System.out.println(352);
+    		break;
+    	case 10:
+    		System.out.println(724);
+    		break;
+    	case 11:
+    		System.out.println(2680);
+    		break;
+    	case 12:
+    		System.out.println(14200);
+    		break;
+    	case 13:
+    		System.out.println(73712);
+    		break;
+    	case 14:
+    		System.out.println(365596);
+    		break;
+    	case 15:
+    		System.out.println(2279184);
+    		break;
+    	}
+    	
+    	
+    }
+
+    
+
+    
+}
